6d9bdb4b4da0658fd2d8d62e17cfd596a40b74a3
Scott <dev@misonline.biz>
2018-12-08 17:46:47 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# RevStackCore.GoogleApis.Core
+
+
+
+
---
+++ RevStackCore.GoogleApis.Core/Context/ApiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace RevStackCore.GoogleApis.Core
+{
+    public class GoogleApiContext
+    {
+        private string _key { get; set; }
+        public string Key
+        {
+            get
+            {
+                return _key;
+            }
+        }
+        public GoogleApiContext(string key)
+        {
+            _key = key;
+        }
+    }
+}
